14ba94405ced0aa14c9b6c2ab7c706a27d894f72
Bigver <91006041+Bigver@users.noreply.github.com>
2023-09-14 00:52:26 +0700

    Update Cart.jsx

--- frontend/src/page/Cart.jsx
+++ frontend/src/page/Cart.jsx
@@ -17,7 +17,7 @@ export default function CartScreen() {
   } = state;
 
   const updateCartHandler = async (item, quantity) => {
-    const { data } = await axios.get(`http://localhost:5000/api/products/${item._id}`);
+    const { data } = await axios.get(`https://react-nodejs-ecommerce.onrender.com/api/products/${item._id}`);
     if (data.countInStock < quantity) {
       window.alert('Sorry. Product is out of stock');
       return;
